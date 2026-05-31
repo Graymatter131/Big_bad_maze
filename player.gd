acb881f8a4a6246c898413035925704ba3676256
Graymatter131 <154765532+Graymatter131@users.noreply.github.com>
2026-05-31 08:55:12 -0600

    old but new

--- player.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-
-var speed = 200
-
-
-func _physics_process(delta: float) -> void:
-	var dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = dir * speed
-	move_and_slide()
